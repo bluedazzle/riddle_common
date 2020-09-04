3bee19a777a3454a728c6132fa7c9ce1723728b4
zhangjianqi <zhangjianqi@bytedance.com>
2020-09-04 22:43:52 +0800

    add docker config
    Change-Id: Iae1413d058ad207fb5578697f7f56373c665f031

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:2.7.13
 ADD . /site
 
 WORKDIR /site
-RUN pip install -r requirements.txt
-CMD uwsgi conf/uwsgi.xml
+RUN pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple && mkdir /logs/
+EXPOSE 8000
---
+++ conf/nginx/conf.d
@@ -0,0 +1,29 @@
+server {
+    client_max_body_size 1024m;
+    listen       8080;
+    server_name  localhost;
+    charset  utf-8;
+
+    location / {
+        include uwsgi_params;
+        uwsgi_pass 10.127.2.5:8000;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    #定义项目静态文件目录，注意alias和root的区别
+    location /static {
+        alias /site/static;
+    }
+
+    error_page 404 /404.html;
+            location = /40x.html {
+        }
+
+    error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+    }
+
+}
