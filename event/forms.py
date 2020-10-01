e28943d23f58f99df9359ca44161a8a7401c5982
root <lyonbot@outlook.com>
2020-10-01 22:21:10 +0800

    fix: wrong default

--- event/forms.py
+++ event/forms.py
@@ -13,7 +13,7 @@ class AdEventForm(forms.ModelForm):
 
     ad_type = forms.CharField(required=True, max_length=9999, error_messages=cash_messages)
     channel = forms.CharField(required=False, max_length=1000)
-    extra = forms.CharField(required=False, max_length=1024, default='')
+    extra = forms.CharField(required=False, max_length=1024)
 
     def save(self, commit=False):
         return super(AdEventForm, self).save(commit)
