e70fcb7ce21061ff63afdd61ff0c8b64cc17c056
goodnighthy <hengyu1213@163.com>
2020-11-15 18:13:32 +0800

    add questions 1

--- question/management/commands/task.py
+++ question/management/commands/task.py
@@ -30,44 +30,73 @@ def song_init(execl, songs=0):
 
 def question_init(execl, questions=0):
     df = pd.read_excel(execl, sheet_name=u'questions', encoding='utf-8')
-    infos = df.ix[:, [u'歌手名', u'歌曲名', u'容易度', u'错误歌曲名']]
+    infos = df.ix[:, [u'id', u'题目顺序', u'题目类型', u'正确答案', u'错误答案']]
     questions_list = []
+    name_dic = {}
     if questions == 0:
-        lines = len(infos[u'歌手名'])
+        lines = len(infos[u'id'])
     else:
         lines = questions
     for line in range(lines):
-        if pd.isna(infos[u'是否完成'][line]) or pd.isna(infos[u'歌手名'][line]) or pd.isna(infos[u'歌曲名'][line]) \
-                or pd.isna(infos[u'容易度'][line]) or pd.isna(infos[u'错误歌曲名'][line]):
+        pics_list = []
+        pics_num = 0
+        try_num = 0
+        try_total = 0
+        if pd.isna(infos[u'id'][line]) or pd.isna(infos[u'题目顺序'][line]) or pd.isna(infos[u'题目类型'][line]) \
+                or pd.isna(infos[u'正确答案'][line]) or pd.isna(infos[u'错误答案'][line]):
             continue
-        url = prefix + pingyin(infos[u'歌手名'][line]).replace('，', '') + '_' + pingyin(infos[u'歌曲名'][line]).replace('，', '') + '.m4a'
-        try:
-            resp = requests.get(str(url), timeout=4)
-        except:
-            print(url)
+        if not name_dic.get(infos[u'正确答案'][line]):
+            name_dic[infos[u'正确答案'][line]] = 1
+
+        while pics_num < 3:
+            pic = infos[u'正确答案'][line] + '/' + infos[u'正确答案'][line] + '-' + str(name_dic[infos[u'正确答案'][line]]) + '.jpg'
+            encode_pic = parse.quote(pic)
+            url = prefix + encode_pic
+            try:
+                resp = requests.get(str(url), timeout=4)
+            except :
+                try_num = try_num + 1
+                name_dic[infos[u'正确答案'][line]] = name_dic[infos[u'正确答案'][line]] + 1
+                if try_num >= 3:
+                    try_num = 0
+                    name_dic[infos[u'正确答案'][line]] = 0
+                continue
+            if resp.status_code != 200:
+                try_num = try_num + 1
+                name_dic[infos[u'正确答案'][line]] = name_dic[infos[u'正确答案'][line]] + 1
+                if try_num >= 3:
+                    try_num = 0
+                    name_dic[infos[u'正确答案'][line]] = 0
+                continue
+            try_num = 0
+            try_total = try_total + 1
+            if try_total == 12:
+                break
+            name_dic[infos[u'正确答案'][line]] = name_dic[infos[u'正确答案'][line]] + 1
+            pics_num = pics_num + 1
+            pics_list.append(url)
+        if pics_num < 3:
             continue
-        if resp.status_code != 200:
-            continue
-        questions_list.append((int(infos[u'容易度'][line]), infos[u'歌曲名'][line], infos[u'错误歌曲名'][line], url))
-    # sorted(questions2_list, key=itemgetter(1))
-    # questions2_list.sort(key=itemgetter(1))
-    questions_list.sort(key=lambda x: (x[0], x[1]))
-    # print questions_list
+        print(pics_list)
+        questions_list.append((infos[u'id'][line], infos[u'题目顺序'][line], infos[u'题目类型'][line],
+                               infos[u'正确答案'][line], infos[u'错误答案'][line], pics_list))
+
+    questions_list.sort(key=lambda x: (x[1]))
     print("total: " + str(len(questions_list)))
-    # for num in range(len(questions1_list)):
-    #     print num, questions1_list[num][1]
+    print(questions_list)
+
 
 
 
 
 if __name__ == "__main__":
-    # question_init(u'questions.xlsx', 100)
+    question_init(u'questions.xlsx', 100)
 
-    pic = '刘亦菲/刘亦菲－1' + '.jpg'
-    encode_pic = parse.quote(pic)
-    url = prefix + encode_pic
-
-    print(url)
-    resp = requests.get(url, timeout=4)
-    if resp.status_code != 200:
-        print("success!")
+    # pic = '刘亦菲/刘亦菲－1' + '.jpg'
+    # encode_pic = parse.quote(pic)
+    # url = prefix + encode_pic
+    #
+    # print(url)
+    # resp = requests.get(url, timeout=4)
+    # if resp.status_code != 200:
+    #     print("success!")
