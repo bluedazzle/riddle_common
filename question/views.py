e6922eeab8417b4b8e5823fd629d4fb09f3124f4
goodnighthy <hengyu1213@163.com>
2020-12-08 01:39:00 +0800

    add continu

--- question/views.py
+++ question/views.py
@@ -146,18 +146,21 @@ class AnswerView(CheckTokenMixin, ABTestMixin, StatusWrapMixin, JsonResponseMixi
         if obj.right_answer_id != aid and self.user.current_level != 1:
             self.user.wrong_count += 1
             self.user.reward_count = 0
+            client_redis_riddle.set(str(self.user.id) + 'continu', self.user.continu_count)
+            self.user.continu_count = 0
             if self.user.current_level == 1185:
                 self.user.current_level = 0
             self.user.current_level += 1
             self.user.save()
             return self.render_to_response(
-                {'answer': False, 'cash': 0, 'reward': False, 'reward_url': '', 'video': video})
+                {'answer': False, 'cash': 0, 'reward': False, 'reward_url': '', 'video': video, 'continu': 0})
 
         if self.user.current_step == round_count:
             self.user.current_step = 0
         self.user.current_step += 1
         self.user.right_count += 1
         self.user.reward_count += 1
+        self.user.continu_count = min(199, self.user.continu_count + 1)
         self.user.cash += cash
         reward = False
         reward_url = ''
@@ -175,7 +178,7 @@ class AnswerView(CheckTokenMixin, ABTestMixin, StatusWrapMixin, JsonResponseMixi
         self.daily_rewards_handler()
         self.user.save()
         return self.render_to_response(
-            {'answer': True, 'cash': cash, 'reward': reward, 'reward_url': reward_url, 'video': video})
+            {'answer': True, 'cash': cash, 'reward': reward, 'reward_url': reward_url, 'video': video, 'continu': self.user.continu_count})
 
 
 class StimulateView(CheckTokenMixin, StatusWrapMixin, JsonResponseMixin, DetailView):
@@ -184,17 +187,29 @@ class StimulateView(CheckTokenMixin, StatusWrapMixin, JsonResponseMixin, DetailV
 
     def post(self, request, *args, **kwargs):
         video = request.GET.get('is_watch_video', '0')
-        if video != '1':
+        resurge = request.GET.get('resurge', '0')
+        if video != '1' and resurge != '1':
             return self.render_to_response()
-        cash = client_redis_riddle.get(str(self.user.id) + 'cash')
-        if cash:
-            try:
-                cash = int(cash)
-                self.user.cash += cash
-                client_redis_riddle.delete(str(self.user.id) + 'cash')
-                self.user.save()
-            except Exception as e:
-                logging.exception(e)
+        if video == '1':
+            cash = client_redis_riddle.get(str(self.user.id) + 'cash')
+            if cash:
+                try:
+                    cash = int(cash)
+                    self.user.cash += cash
+                    client_redis_riddle.delete(str(self.user.id) + 'cash')
+                except Exception as e:
+                    logging.exception(e)
+        if resurge == "1":
+            continu = client_redis_riddle.get(str(self.user.id) + 'continu')
+            if continu:
+                try:
+                    continu = int(continu)
+                    self.user.continu_count = continu
+                    client_redis_riddle.delete(str(self.user.id) + 'contins')
+                except Exception as e:
+                    logging.exception(e)
+        self.user.save()
+
         return self.render_to_response()
 
 
