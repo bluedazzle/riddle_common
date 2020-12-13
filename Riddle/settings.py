1c9b427834b6a64b37f764d28951f99bb4c912cf
zhangjianqi <zhangjianqi@bytedance.com>
2020-12-13 16:58:05 +0800

    add cors support
    Change-Id: I57f2a4878af21d531eebeafeaf524037da6e37ed

--- Riddle/settings.py
+++ Riddle/settings.py
@@ -54,14 +54,15 @@ INSTALLED_APPS = [
     'baseconf',
     'event',
     'task',
-   'django_prometheus',
+    'corsheaders',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -153,10 +154,13 @@ STATIC_ROOT = './static/'
 
 STATIC_MEDIA = './static/'
 
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = ("http://cai-ta.ecdn.plutus-cat.com", "https://cai-ta.ecdn.plutus-cat.com")
-# CORS_ALLOWED_ORIGINS = ["http://cai-ta.ecdn.plutus-cat.com",
-#                         "https://cai-ta.ecdn.plutus-cat.com"]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+"http://cai-ta.ecdn.plutus-cat.com", "https://cai-ta.ecdn.plutus-cat.com", "http://appweb.ecdn.plutus-cat.com/",
+"https://appweb.ecdn.plutus-cat.com/")
+CORS_ALLOWED_ORIGINS = ["http://cai-ta.ecdn.plutus-cat.com",
+                        "https://cai-ta.ecdn.plutus-cat.com", "http://appweb.ecdn.plutus-cat.com/",
+                        "https://appweb.ecdn.plutus-cat.com/"]
 
 # init dependency
 config_client_redis_zhz()
